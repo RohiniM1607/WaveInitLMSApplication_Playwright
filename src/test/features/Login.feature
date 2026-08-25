aa6e7d37c1f5e0d1a1ad5e689647c0e3efaec509
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-08-25 09:16:00 +0530

    Trainer and participant module positive test case completed

--- src/test/features/Login.feature
+++ src/test/features/Login.feature
@@ -5,18 +5,23 @@ Feature: VIGNESHWARAN 24th August 2026
   Background: User is on the login page
     Given The user launches the application
 
-  Scenario: User should be able to login with valid credentials
+  Scenario Outline: User should be able to login with valid credentials
+    When The user clicks the "<role>" login button
     When The user enters valid username and password
     And The user clicks on the login button
     Then The the dashboard header should be displayed with the text "Welcome back"
 
-  
+    Examples:
+      | role    |
+      | Trainer |
+      | Learner |
+
   Scenario Outline: Application should display proper error message on invalid login
     When The user enters the "<testcase>" username and password
     And The user clicks on the login button
     Then The application should display the "<error message>"
 
     Examples:
-      | testcase | error message |
+      | testcase | error message             |
       | invalid  | Invalid email or password |
-      | empty    | Validation message |
+      | empty    | Validation message        |
--- src/test/steps/LoginPageSteps.ts
+++ src/test/steps/LoginPageSteps.ts
@@ -3,13 +3,16 @@ import { CustomWorld } from "../../main/support/CustomWorld";
 import { expect } from "playwright/test";
 import loginData from "../../resources/data/loginDataset.json";
 
+let selectedRole: string;
+
 Given('The user launches the application', async function (this: CustomWorld) {
     await this.loginPage.navigate();
 });
        
 When('The user enters valid username and password', async function (this: CustomWorld) {
-    await this.loginPage.fillUsername(loginData.valid.username);
-    await this.loginPage.fillPassword(loginData.valid.password);
+    const testData = loginData[`valid${selectedRole}` as keyof typeof loginData];
+    await this.loginPage.fillUsername(testData.username);
+    await this.loginPage.fillPassword(testData.password);
 });
               
 When('The user clicks on the login button', async function (this: CustomWorld) {
@@ -18,7 +21,7 @@ When('The user clicks on the login button', async function (this: CustomWorld) {
               
               
 When('The user enters the {string} username and password', async function (this: CustomWorld, testcase : string) {
-    const testData = loginData[testcase as keyof typeof loginData];
+    const testData = loginData[`${testcase}Credentials` as keyof typeof loginData];
     await this.loginPage.fillUsername(testData.username);
     await this.loginPage.fillPassword(testData.password);
 });
@@ -26,10 +29,9 @@ When('The user enters the {string} username and password', async function (this:
 Then('The application should display the {string}', async function (this: CustomWorld, expectedMessage: string) {
     
     if(expectedMessage === "Validation message") { 
-        this.loginPage.verifyValidity().then((validity) => {
-            expect(validity.isNameValid).toBe(false);
-            expect(validity.isDescriptionValid).toBe(false);
-        })
+        const validity = await this.loginPage.verifyValidity();
+        expect(validity.isNameValid).toBe(false);
+        expect(validity.isDescriptionValid).toBe(false);
     }
     else{
         const actualMessage = await this.loginPage.getErrorMessage();
@@ -42,4 +44,9 @@ Then('The application should display the {string}', async function (this: Custom
 Then('The the dashboard header should be displayed with the text {string}', async function(this: CustomWorld, expectedText: string)  {
     const actualText = await this.dashboardPage.getDashboardHeaderText();
     expect(actualText?.startsWith(expectedText)).toBe(true);
+})
+
+When('The user clicks the {string} login button', async function (this: CustomWorld, role: string)  {
+    selectedRole = role;
+    await this.loginPage.clickLogin(role);
 })
