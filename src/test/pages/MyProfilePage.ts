28bd267a2fd198da467a491d9ca2ac4cb821947e
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-08-28 17:57:33 +0530

    Profile scenarios corrected

--- src/test/pages/MyProfilePage.ts
+++ src/test/pages/MyProfilePage.ts
@@ -3,19 +3,24 @@ import { BasePage } from "./BasePage"
 
 export class MyProfilePage extends BasePage {
 
+    private newPage: any;
     private saveLinksButton = this.page.locator('//button[@class = "pfd-btn-primary"]');
     private editSocialLinksButton = this.page.locator('//div[text() = "Social Links"]/following::button[1]');
 
     async fillSocialLink(socialMedia: string, socialLink: string) {
         logger.info(`Filling social link for ${socialMedia}: ${socialLink}`);
-        const locator = this.page.locator(`//input[contains(@placeholder, "${socialMedia}")]`);
+        const locator = this.page.locator(`//input[contains(@placeholder, "${socialMedia.toLocaleLowerCase()}")]`);
         await this.fill(locator, socialLink);
     }
 
     async clickSocialLink(socialMedia: string) {
         logger.info(`Clicking on ${socialMedia} social link`);
-        const locator = this.page.locator(`//a[contains(@href, "${socialMedia.toLocaleLowerCase()}")]`);
-        await this.click(locator);
+        
+        [this.newPage] = await Promise.all([
+            this.page.context().waitForEvent('page'),
+            this.page.locator(`//a[contains(@href, "${socialMedia.toLocaleLowerCase()}")]`).click()
+        ]);
+        await this.newPage.waitForLoadState();
     }
 
     async clickSaveLinksButton() {
@@ -30,12 +35,13 @@ export class MyProfilePage extends BasePage {
 
     async isSocialLinkVisible(socialMedia: string): Promise<boolean> {
         logger.info(`Checking visibility of ${socialMedia} social link`);
-        const locator = this.page.locator(`//a[contains(@href, "${socialMedia.toLocaleLowerCase()}")]`);
+        const locator = this.page.locator(`//a[contains(@href, "${socialMedia}")]`);
+        logger.info(`Locator for ${socialMedia} social link: ${locator}`);
         return await this.isVisible(locator);
     }
 
     async getCurrentPageUrl() {
         logger.info("Getting current page URL");
-        return this.page.url();
+        return this.newPage.url();
     }
 }
